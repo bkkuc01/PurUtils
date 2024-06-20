c8032f6eb6ac4dc0361993339639660e91652be0
bkkuc01 <bkkuc@bk.ru>
2024-06-21 03:11:32 +0500

    f

--- src/main/java/pl/bkkuc/purutils/other/Updater.java
+++ src/main/java/pl/bkkuc/purutils/other/Updater.java
@@ -2,48 +2,49 @@ package pl.bkkuc.purutils.other;
 
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.bukkit.Bukkit;
 import pl.bkkuc.purutils.UtilPlugin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.HttpURLConnection;
 import java.net.URI;
-import java.util.Optional;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Updater {
     private static final Logger LOGGER = LogManager.getLogger(UtilPlugin.class);
     private static final URI URI_Releases = URI.create("https://api.github.com/repos/bkkuc01/PurUtils/releases/latest");
 
     public static void check(final String currentVersion) {
-        LOGGER.info("Проверка обновления...");
+        LOGGER.info("Checking update...");
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            try (CloseableHttpResponse response = httpClient.execute(
-                    RequestBuilder.get()
-                            .setUri(URI_Releases)
-                            .build()
-            )) {
-                HttpEntity entity = response.getEntity();
-                if (Optional.ofNullable(entity).isPresent()) {
-                    UpdateResponse lastRelease = new Gson().fromJson(
-                            EntityUtils.toString(entity),
-                            UpdateResponse.class);
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(URI_Releases.toString()).openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                    StringBuilder response = new StringBuilder();
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+
+                    UpdateResponse lastRelease = new Gson().fromJson(response.toString(), UpdateResponse.class);
 
                     if (isNewVersion(currentVersion, lastRelease.getTagName())) {
                         LOGGER.info("\n---------------------------------------------------" +
                                         "\nДоступно обновление!" +
                                         "\nТекущая версия: {}" +
                                         "\nНовая версия: {}" +
-                                        "\nСкачать можно тут: https://github.com/bkkuc01/PurUti;s/releases/{}" +
+                                        "\nСкачать можно тут: https://github.com/bkkuc01/PurUtils/releases/{}" +
                                         "\nСведения об обновлении: \n{}" +
                                         "\n---------------------------------------------------",
                                 currentVersion,
@@ -51,9 +52,12 @@ public class Updater {
                                 lastRelease.getTagName(),
                                 lastRelease.getBody());
                         Bukkit.getPluginManager().disablePlugin(UtilPlugin.getInstance());
-
-                    } else LOGGER.info("Обновления не найдены.");
-                } else LOGGER.warn("Не удалось проверить версию | {}", response);
+                    } else {
+                        LOGGER.info("Обновления не найдены.");
+                    }
+                }
+            } else {
+                LOGGER.warn("Не удалось проверить версию | HTTP response code: {}", responseCode);
             }
         } catch (Exception e) {
             StringWriter stringWriter = new StringWriter();
